ff74ab7a8695cb182e05ceb53d6b0dc027b05e9b
devil1012 <devil101281@gmail.com>
2021-11-09 10:36:58 +0800

    Initial commit

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,12 @@
 const{Pool}=require('pg')
+const isProduction=process.env.NODE_ENV==="production"
 const pool=new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'crown_53',
-    password:'0000',
-    port:5432
+    // user:'postgres',
+    // host:'localhost',
+    // database:'crown_53',
+    // password:'0000',
+    // port:5432
+    connectionString:isProduction?process.env.DATABASE:'postgresql://postgres:0000@localhost:5432/crown_53'
 })
 pool.query('SELECT*from category_53',(err,res)=>{
     console.log(JSON.stringify(res.rows));
